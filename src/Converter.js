c4b8b26c38311956e6ca3453aa5381816cb0c8d0
MFogleman <marcfogleman@gmail.com>
2016-10-15 09:14:40 -0400

    Initial build complete

--- src/Converter.js
+++ src/Converter.js
@@ -1,7 +1,6 @@
-/* eslint-disable*/
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 var marked = require('marked');
+
 marked.setOptions({
     renderer: new marked.Renderer(),
     gfm: true,
@@ -17,40 +16,45 @@ class Converter extends Component{
     constructor(props) {
         super(props);
         this.state = {rawText: ""};
-        this.state = {fancyText: ""};
         this.textInput = this.textInput.bind(this);
         this.markdownText = this.markdownText.bind(this);
     }
+    
     textInput(e) {  
         this.setState({rawText:e.target.value});            
     }
-    markdownText(){
+    
+    markdownText() {
         if (this.state.rawText){
             var rawMarkdown = marked(this.state.rawText, {sanitize:true} || "");
             return {__html: rawMarkdown}
         }
-        return {__html:""}
+        return {__html:"<br>and see your compiled Markdown here!"}
     }
 
     render() {
         return (
             <div className="converter">
+                
+                <div className="header">
+                    <h1>Markdown Preview</h1>
+                </div>
+                
                 <textarea 
                     name="inputarea"
                     className="input" 
-                    placeholder="Enter your raw markdown"
+                    placeholder="Enter your raw Markdown here..."
                     onChange={this.textInput}
-                    value={this.state.rawText}
                 />
                 
                 <div
                     className="output"
                     dangerouslySetInnerHTML={this.markdownText()}
-                >
-                
-                </div>                                     
+                />
+                                                     
             </div>
         )
     }
 }
+
 export default Converter;    
